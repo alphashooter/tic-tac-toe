51bed1be9ecde5cc71faac9d788883c6a074bfd3
Andrey Strelkov <a.p.strelkov@gmail.com>
2019-07-18 12:11:26 +0300

    flask example

--- web.py
+++ web.py
@@ -4,9 +4,22 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# http://localhost:5000/
 @app.route('/')
 def index():
-    return 'hello!'
+    return 'Hello, World!'
+
+
+# http://localhost:5000/hello/Bob
+@app.route('/hello/<name>')
+def hello1(name):
+    return f'Hello, {name}!'
+
+
+# http://localhost:5000/hello/James_Bond
+@app.route('/hello/<name>_<surname>')
+def hello2(name, surname):
+    return f'{surname}. {name} {surname}.'
 
 
 app.run()
