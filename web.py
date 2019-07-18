e17d75462a8f65eb08b4e65676afdea2461b1bae
Andrey Strelkov <a.p.strelkov@gmail.com>
2019-07-18 12:22:41 +0300

    flask example

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response, make_response
 
 
 app = Flask(__name__)
@@ -20,6 +20,17 @@ def hello1(name):
 @app.route('/hello/<name>_<surname>')
 def hello2(name, surname):
     return f'{surname}. {name} {surname}.'
+
+
+# Для работы необходимо, чтобы в проекте лежала картинка с именем pic.jpg
+# http://localhost:5000/image/pic.jpg
+@app.route('/picture/<path>')
+def picture(path):
+    with open(path, 'rb') as file:
+        data = file.read()
+    response: Response = make_response(data)
+    response.headers['Content-Type'] = 'image/jpeg'
+    return response
 
 
 app.run()
